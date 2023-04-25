1e15fd03223afe27469c94a436956971a35aaed5
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2023-04-25 15:25:22 +0200

    Fix netlify

--- lib/kontentClient.ts
+++ lib/kontentClient.ts
@@ -8,7 +8,7 @@ import { HeroUnit, Homepage } from '../models';
 const sourceTrackingHeaderName = 'X-KC-SOURCE'
 
 const deliveryClient = createDeliveryClient({
-  projectId: process.env.KONTENT_PROJECT_ID || "975bf280-fd91-488c-994c-2f04416e5ee3",
+  environmentId: process.env.KONTENT_ENVIRONMENT_ID || "975bf280-fd91-488c-994c-2f04416e5ee3",
   globalHeaders: (_queryConfig) => [
     {
       header: sourceTrackingHeaderName,
