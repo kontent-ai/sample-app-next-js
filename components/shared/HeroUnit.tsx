b9905495e1537d98b5168cc611ec3dc8e1600ecf
Jiri Lojda <lojdaj@seznam.cz>
2023-04-26 10:35:15 +0200

    Add smartlink attributes to the new components

--- components/shared/HeroUnit.tsx
+++ components/shared/HeroUnit.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { contentTypes } from "../../models";
 import { HeroUnit } from "../../models/content-types/component___hero_unit"
 import { HeroImage } from "../landingPage/ui/heroImage";
 
@@ -8,8 +9,12 @@ type Props = Readonly<{
 
 export const HeroUnitComponent: FC<Props> = props => (
   <HeroImage url={props.item.elements.backgroundImage.value[0]?.url}>
-    <h1>{props.item.elements.title.value}</h1>
-    <h2>{props.item.elements.subtitle.value}</h2>
+    <h1 data-kontent-element-codename={contentTypes.component___hero_unit.elements.title.codename}>
+      {props.item.elements.title.value}
+    </h1>
+    <h2 data-kontent-element-codename={contentTypes.component___hero_unit.elements.subtitle.codename}>
+      {props.item.elements.subtitle.value}
+    </h2>
   </HeroImage>
 );
 
--- components/shared/RichTextContent.tsx
+++ components/shared/RichTextContent.tsx
@@ -2,6 +2,7 @@ import { nodeParse } from "@kontent-ai/rich-text-resolver/dist/cjs/src/parser/no
 import { transformToPortableText } from "@kontent-ai/rich-text-resolver/dist/cjs/src/transformers/portable-text-transformer";
 import { PortableText } from "@portabletext/react";
 import { FC } from "react";
+import { contentTypes } from "../../models";
 import { RichTextContent } from "../../models/content-types/component___rich_text_content"
 
 type Props = Readonly<{
@@ -11,5 +12,9 @@ type Props = Readonly<{
 export const RichTextContentComponent: FC<Props> = props => {
   const portableText = transformToPortableText(nodeParse(props.item.elements.content.value));
 
-  return <PortableText value={portableText} />
+  return (
+    <div data-kontent-element-codename={contentTypes.component___rich_text_content.elements.content.codename}>
+      <PortableText value={portableText} />
+    </div>
+  );
 }
