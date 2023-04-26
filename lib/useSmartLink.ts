4d609b067656be8336809c7cdd59d2eb7c22df65
Jiri Lojda <lojdaj@seznam.cz>
2023-04-26 11:37:24 +0200

    Update languageCodename in smartLink sdk

--- lib/useSmartLink.ts
+++ lib/useSmartLink.ts
@@ -5,7 +5,7 @@ export const useSmartLink = () => useEffect(() => {
   const sdk = KontentSmartLink.initialize({
     defaultDataAttributes: {
       projectId: "2cedc519-a547-01ba-dde7-b01e00b909a1",
-      languageCodename: "en-US",
+      languageCodename: "default",
     }
   });
 
