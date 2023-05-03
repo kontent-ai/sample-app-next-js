52b3e36114ca719b7ef977ae2d68902777ab5e8f
Jiri Lojda <lojdaj@seznam.cz>
2023-05-03 10:43:42 +0200

    Make hero images priority images (improves lcp)

--- components/landingPage/ui/heroImage.tsx
+++ components/landingPage/ui/heroImage.tsx
@@ -18,6 +18,7 @@ export const HeroImage: FC<Props> = props => (
       alt="Hero image"
       fill
       className="object-cover"
+      priority
     />
     <div className="relative w-full h-full flex justify-start items-end">
       <div className="backdrop-contrast-100 bg-white/70 p-10 rounded-tr-lg backdrop-opacity-5">
