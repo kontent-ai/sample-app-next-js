0c22aaa76c929ca4a44379d9cfae5c64c9a71c62
Ondřej Chrastina <ondrejch@kontent.ai>
2023-05-03 17:12:14 +0200

    preview bar

--- components/shared/ui/menu.tsx
+++ components/shared/ui/menu.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC } from "react";
 
 type Link = Readonly<{
@@ -11,21 +12,26 @@ type Props = Readonly<{
 }>;
 
 export const Menu: FC<Props> = props => (
-  <menu className="w-full h-16 shadow-md grow-0 px-10 py-1 flex flex-auto shrink-0 justify-end items-center gap-x-5 bg-green-300">
-    {props.links.map(link => (
-      <li
-        key={link.title}
-        className="h-full"
-      >
-        <Link
-          className="h-full px-5 flex items-center cursor-pointer rounded hover:bg-white"
-          href={link.url}
+  <div className="flex flex-auto w-full bg-green-300 justify-between shadow-md items-center px-10 grow-0 shrink-0">
+    <div>
+      <span className="self-start h-full">{useRouter().isPreview ? "Preview" : "Published"}</span>
+    </div>
+    <menu className="h-16 grow-0 py-1 flex flex-auto shrink-0 justify-end items-center gap-x-5">
+      {props.links.map(link => (
+        <li
+          key={link.title}
+          className="h-full"
         >
-          {link.title}
-        </Link>
-      </li>
-    ))}
-  </menu>
+          <Link
+            className="h-full px-5 flex items-center cursor-pointer rounded hover:bg-white"
+            href={link.url}
+          >
+            {link.title}
+          </Link>
+        </li>
+      ))}
+    </menu>
+  </div>
 );
 
 Menu.displayName = "Menu";
