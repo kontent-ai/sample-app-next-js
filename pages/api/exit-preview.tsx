75f3fc407a0ad2a4d6754c676f8e55e8f4ad361f
Ondřej Chrastina <ondrejch@kontent.ai>
2023-05-03 17:24:23 +0200

    exit preview functionality

---
+++ pages/api/exit-preview.tsx
@@ -0,0 +1,15 @@
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = (req, res) => {
+    console.log("Exiting preview");
+    // Exit the current user from "Preview Mode". This function accepts no args.
+    res.clearPreviewData();
+  
+    // Redirect the user back to the index page.
+    // Might be implemented return URL by the query string.
+
+    const redirect = req.query['redirect']
+    res.redirect("/");
+  }
+
+  export default handler;
