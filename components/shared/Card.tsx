dfe4ead400b383f372cf347bf68ddab6a7abda70
Jiri Lojda <lojdaj@seznam.cz>
2023-05-05 09:56:49 +0200

    Rework card and cardStack components to look better

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -9,19 +9,21 @@ type Props = Readonly<{
 export const CardComponent: FC<Props> = props => {
   const image = props.item.elements.cardImage.value[0];
   return (
-    <figure className="w-64 h-96 rounded shadow">
-      {image && (
-        <Image
-          src={image.url}
-          alt={props.item.elements.title.value}
-          width={300}
-          height={200}
-        />
-      )}
-      <div className="p-4">
-        <b>{props.item.elements.title.value}</b>
+    <figure className="flex items-center gap-1 w-full m-0 h-72">
+      <div className="p-4 w-2/3">
+        <h3>{props.item.elements.title.value}</h3>
         {props.item.elements.message.value}
       </div>
+      {image && (
+        <div className="h-full w-1/3 relative not-prose rounded-3xl overflow-hidden">
+          <Image
+            src={image.url}
+            alt={props.item.elements.title.value}
+            fill
+            className="object-cover"
+          />
+        </div>
+      )}
     </figure>
   );
 }
--- components/shared/CardStack.tsx
+++ components/shared/CardStack.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { CardStack } from "../../models"
 import { CardComponent } from "./Card";
 
@@ -6,14 +6,51 @@ type Props = Readonly<{
   item: CardStack;
 }>;
 
-export const CardStackComponent: FC<Props> = props => (
-  <div className="rounded border-black p-7">
-    <b>{props.item.elements.title.value}</b>
-    {props.item.elements.message.value}
-    <div className="flex flex-wrap gap-6">
-      {props.item.elements.stack.linkedItems.map(item => (
-        <CardComponent key={item.system.id} item={item} />
-      ))}
+export const CardStackComponent: FC<Props> = props => {
+  const [cardIndex, setCardIndex] = useState(0);
+
+  const currentCard = props.item.elements.stack.linkedItems[cardIndex];
+
+  if (!currentCard) {
+    return null;
+  }
+
+  return (
+    <div className="p-7">
+      <h2>{props.item.elements.title.value}</h2>
+      {props.item.elements.message.value}
+      <section className="py-11">
+        <Headers
+          headers={props.item.elements.stack.linkedItems.map(item => item.elements.title.value)}
+          onHeaderSelected={setCardIndex}
+          selectedHeaderIndex={cardIndex}
+        />
+        <div className="pt-3">
+          <CardComponent item={currentCard} />
+        </div>
+      </section>
     </div>
-  </div>
-);
+  );
+};
+
+type HeadersProps = Readonly<{
+  headers: ReadonlyArray<string>;
+  selectedHeaderIndex: number;
+  onHeaderSelected: (headerIndex: number) => void;
+}>;
+
+const Headers: FC<HeadersProps> = props => (
+  <menu className="flex gap-6 border-b-2 border-b-gray-100">
+    {props.headers.map((header, i) => (
+      <li
+        key={i}
+        className={`overflow-hidden h-full m-0 shrink text-ellipsis flex justify-center items-center cursor-pointer border-b-green-600 ${props.selectedHeaderIndex === i ? "border-b-2" : ""}`}
+        onClick={() => props.onHeaderSelected(i)}
+      >
+        {header}
+      </li>
+    ))}
+  </menu>
+)
+
+const wrapIndex = ({ index, length }: Readonly<{ index: number; length: number }>) => index < 0 ? length + index : index % length;
