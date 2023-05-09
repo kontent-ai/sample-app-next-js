6c9a95ad0f6402e6cf43c3c54a42855bd935676e
Jiri Lojda <lojdaj@seznam.cz>
2023-05-09 17:46:07 +0200

    Remove exitPreview button

--- components/shared/ui/menu.tsx
+++ components/shared/ui/menu.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { FC } from "react";
-import { useSiteCodename } from "../siteCodenameContext";
 import { mainColorBgClass } from "../../../lib/constants/colors";
+import { useSiteCodename } from "../siteCodenameContext";
 
 type Link = Readonly<{
   url: string;
@@ -14,25 +13,11 @@ type Props = Readonly<{
 }>;
 
 export const Menu: FC<Props> = props => {
-  const router = useRouter();
   const siteCodename = useSiteCodename();
 
   return (
     <div className={`flex flex-auto h-16 w-full ${mainColorBgClass[siteCodename]} justify-between shadow-md items-center px-10 grow-0 shrink-0`}>
-      <div className="h-full flex gap-x-5 py-1">
-        {router.isPreview && (
-          <div
-            className="h-full"
-          >
-            <Link
-              className="h-full px-5 cursor-pointer rounded hover:bg-white flex items-center"
-              href="/api/exit-preview"
-            >
-              Exit preview
-            </Link>
-          </div>
-        )}
-      </div>
+      <div />
       <menu className="h-full grow-0 py-1 flex flex-auto shrink-0 justify-end items-center gap-x-5">
         {props.links.map(link => (
           <li
