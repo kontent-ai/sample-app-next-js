a8987cb22a630b3011ba6851f62de64a6b609aff
Jiri Lojda <lojdaj@seznam.cz>
2023-05-12 09:30:53 +0200

    Render add-buttons in cardStack to the right

--- components/shared/CardStack.tsx
+++ components/shared/CardStack.tsx
@@ -53,7 +53,7 @@ const Headers: FC<HeadersProps> = props => {
           className={`overflow-hidden h-full m-0 shrink text-ellipsis flex justify-center items-center cursor-pointer ${mainColorBorderClass[siteCodename]} ${props.selectedHeaderIndex === i ? "border-b-2" : ""}`}
           onClick={() => props.onHeaderSelected(i)}
           {...createItemSmartLink(header.id, true)}
-          {...createRelativeAddSmartLink("after")}
+          {...createRelativeAddSmartLink("after", "right")}
         >
           {header.label}
         </li>
