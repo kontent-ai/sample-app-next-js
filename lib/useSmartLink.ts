b9e1b88a28d756c08c2aa9b9a740ffbe4d98ac8c
Ivan Kiral <kiralivan@gmail.com>
2023-05-24 11:07:00 +0200

    Upate smartlink

--- lib/useSmartLink.ts
+++ lib/useSmartLink.ts
@@ -1,17 +1,23 @@
 import KontentSmartLink from "@kontent-ai/smart-link"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export const useSmartLink = () => useEffect(() => {
-  const envId = process.env.NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID;
-  if (!envId) {
-    throw new Error("Cannot initialize smart-link sdk without 'NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID' environment variable.");
-  }
-  const sdk = KontentSmartLink.initialize({
-    defaultDataAttributes: {
-      projectId: envId,
-      languageCodename: "default",
+export const useSmartLink = () => {
+  const [sdk, setSdk] = useState<KontentSmartLink | null>(null);
+
+  useEffect(() => {
+    const envId = process.env.NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID;
+    if (!envId) {
+      throw new Error("Cannot initialize smart-link sdk without 'NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID' environment variable.");
     }
-  });
+    setSdk(KontentSmartLink.initialize({
+      defaultDataAttributes: {
+        projectId: envId,
+        languageCodename: "default",
+      }
+    }));
 
-  return () => sdk.destroy()
-}, []);
+    return () => sdk?.destroy()
+  }, [setSdk, sdk]);
+
+  return sdk;
+}
