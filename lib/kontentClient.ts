dc8a1e3111029b1f5778b753492b655673242795
Ivan Kiráľ <54802833+IvanKiral@users.noreply.github.com>
2023-05-26 09:35:01 +0200

    showcase preserve state (#10)
    * change home to preserve state
    
    * preserve state
    
    * Upate smartlink
    
    * fetch data using api routes
    
    * wait for preview content to be ready
    
    * remove unnecesary preview prop
    
    * Fix sdk

--- lib/kontentClient.ts
+++ lib/kontentClient.ts
@@ -3,7 +3,8 @@ import { PerCollectionCodenames } from './routing';
 import { siteCodename } from './utils/env';
 import { contentTypes, Product, WebSpotlightRoot } from '../models';
 
-const sourceTrackingHeaderName = 'X-KC-SOURCE'
+const sourceTrackingHeaderName = 'X-KC-SOURCE';
+
 const envId = process.env.NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID;
 if (!envId) {
   throw new Error("Missing 'NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID' environment variable.");
@@ -15,7 +16,7 @@ const deliveryClient = createDeliveryClient({
     {
       header: sourceTrackingHeaderName,
       value: `${process.env.APP_NAME || "n/a"};${process.env.APP_VERSION || "n/a"}`,
-    },
+    }
   ],
   propertyNameResolver: camelCasePropertyNameResolver,
   proxy: {
@@ -70,6 +71,7 @@ export const getHomepage = (usePreview: boolean) =>
     .collection(siteCodename)
     .queryConfig({
       usePreviewMode: usePreview,
+      waitForLoadingNewContent: usePreview
     })
     .depthParameter(10)
     .toPromise()
