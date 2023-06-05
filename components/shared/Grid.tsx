3a98a5c1eb6617157701e0990b80c3a3c31389b3
Daniel Pokorny <danielp@kontent.ai>
2023-06-05 13:32:05 +0200

    rename grid component, add resolution

--- components/shared/Grid.tsx
+++ components/shared/Grid.tsx
@@ -5,7 +5,7 @@ type Props = Readonly<{
   item: Block_Grid
 }>
 
-export const MilestonesComponent: FC<Props> = props => {
+export const GridComponent: FC<Props> = props => {
   return (
     <div className="grid bg-green-300 w-screen relative left-2/4 right-2/4 px-7 [margin-left:-50vw] py-7 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 place-items-center">
       {props.item.elements.gridItems.linkedItems.map(link => (
