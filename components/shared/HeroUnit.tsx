08a7c936bd95fd08f4105cf9b89b3b1baf8644c8
Ivan Kiral <kiralivan@gmail.com>
2023-06-16 11:51:27 +0200

    update heroUnit

--- components/shared/HeroUnit.tsx
+++ components/shared/HeroUnit.tsx
@@ -2,24 +2,33 @@ import { FC } from "react";
 import { Block_HeroUnit, contentTypes } from "../../models";
 import { HeroImage } from "../landingPage/ui/heroImage";
 import { createElementSmartLink } from "../../lib/utils/smartLinkUtils";
+import { mainColorBgClass } from "../../lib/constants/colors";
+import { useSiteCodename } from "./siteCodenameContext";
 
 type Props = Readonly<{
   item: Block_HeroUnit;
 }>;
 
-export const HeroUnitComponent: FC<Props> = props => (
-  <HeroImage
-    url={props.item.elements.backgroundImage.value[0]?.url}
-    itemId={props.item.system.id}
-  >
-    <h1 {...createElementSmartLink(contentTypes.hero_unit.elements.title.codename)}>
-      {props.item.elements.title.value}
-    </h1>
-    <h2 {...createElementSmartLink(contentTypes.hero_unit.elements.subtitle.codename)}>
-      {props.item.elements.subtitle.value}
-    </h2>
-  </HeroImage>
-);
+export const HeroUnitComponent: FC<Props> = props => {
+  const siteCodename = useSiteCodename();
+  return (
+    <HeroImage
+      url={props.item.elements.backgroundImage.value[0]?.url}
+      itemId={props.item.system.id}
+    >
+      <div className={`py-1 px-3 w-full md:w-fit ${mainColorBgClass[siteCodename]}  opacity-[85%]`}>
+        <h1 className="m-0 text-3xl tracking-wide font-semibold"  {...createElementSmartLink(contentTypes.hero_unit.elements.title.codename)}>
+          {props.item.elements.title.value}
+        </h1>
+      </div>
+      <div className="py-1 px-3 w-full bg-white opacity-90">
+        <h2 className="m-0 text-xl font-medium break-words hyphens-auto" lang='en' {...createElementSmartLink(contentTypes.hero_unit.elements.subtitle.codename)}>
+          {props.item.elements.subtitle.value}
+        </h2>
+      </div>
+    </HeroImage>
+  );
+}
 
 HeroUnitComponent.displayName = "HeroUnitComponent";
 
