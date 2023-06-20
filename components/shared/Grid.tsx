2e5a180a0f91b28200f032e372bcfe1e21383d13
Ondřej Chrastina <ondrejch@kontent.ai>
2023-06-20 14:38:55 +0200

    unify width for container to  max-w-screen-xl from tailwind

--- components/shared/Grid.tsx
+++ components/shared/Grid.tsx
@@ -1,19 +1,25 @@
 import React, { FC } from 'react'
 import { Block_Grid } from '../../models'
+import { mainColorBgClass } from '../../lib/constants/colors'
+import { useSiteCodename } from './siteCodenameContext'
 
 type Props = Readonly<{
   item: Block_Grid
 }>
 
 export const GridComponent: FC<Props> = props => {
+  const siteCodename = useSiteCodename();
+
   return (
-    <div className="grid bg-green-300 w-screen relative left-1/2 right-1/2 px-7 [margin-left:-50vw] py-7 text-gray-900 sm:grid-cols-2 md:grid-cols-3">
-      {props.item.elements.gridItems.linkedItems.map((link) => (
-        <div className="p-4 flex flex-col text-center items-center" key={link.system.id}>
-          <div className="font-bold text-3xl">{link.elements.title.value}</div>
-          <div>{link.elements.subtitle.value}</div>
-        </div>
-      ))}
+    <div className={`${mainColorBgClass[siteCodename]} w-screen relative left-1/2 right-1/2 [margin-left:-50vw]`}>
+      <div className="grid mx-auto w-full max-w-screen-xl py-7 text-gray-900 sm:grid-cols-2 md:grid-cols-3">
+        {props.item.elements.gridItems.linkedItems.map((link) => (
+          <div className="p-4 flex flex-col text-center items-center" key={link.system.id}>
+            <div className="font-bold text-3xl">{link.elements.title.value}</div>
+            <div>{link.elements.subtitle.value}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
