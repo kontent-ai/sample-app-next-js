858ea99aac866d5fe3a3b9a74ea9e1d801fb2b62
Ondřej Chrastina <ondrejch@kontent.ai>
2023-06-27 15:04:28 +0200

    article listing - use categories from model + reuse total count for pagination

--- pages/articles/category/[category]/page/[page].tsx
+++ pages/articles/category/[category]/page/[page].tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
-import { Article, ArticleType, Block_Navigation, ProductCategory, WSL_Page, contentTypes } from "../../../../../models";
+import { Article, Block_Navigation, WSL_Page } from "../../../../../models";
 import { ValidCollectionCodename } from "../../../../../lib/types/perCollection";
 import { GetStaticProps } from "next";
-import { getItemsCount, getArticlesForListing, getItemByCodename, getSiteMenu, getArticlesCountByCategory } from "../../../../../lib/kontentClient";
+import { getItemsTotalCount, getArticlesForListing, getItemByCodename, getSiteMenu, getArticlesCountByCategory } from "../../../../../lib/kontentClient";
 import { siteCodename } from "../../../../../lib/utils/env";
 import { PerCollectionCodenames } from "../../../../../lib/routing";
-import ArticlesPage from "..";
 import { ArticlePageSize } from "../../../../../lib/constants/paging";
+import { isArticleType, ArticleTypeWithAll, categoryFilterSource, ArticleListingUrlQuery } from "../../../../../lib/utils/articlesListing";
+import ArticlesPage from "..";
 
 type Props = Readonly<{
   siteCodename: ValidCollectionCodename;
@@ -20,7 +21,7 @@ const ArticlesPagingPage: FC<Props> = props => {
   return <ArticlesPage {...props} />
 }
 
-export const getStaticProps: GetStaticProps<Props> = async context => {
+export const getStaticProps: GetStaticProps<Props, ArticleListingUrlQuery> = async context => {
   const pageCodename: PerCollectionCodenames = {
     ficto_healthtech: "articles",
     ficto_healthtech_imaging: null,
@@ -43,10 +44,18 @@ export const getStaticProps: GetStaticProps<Props> = async context => {
   //   }
   // }
 
-  const articles = await getArticlesForListing(!!context.preview, pageNumber, context.params?.category as string ?? 'all');
+  const selectedCategory = context.params?.category;
+  if (!isArticleType(selectedCategory)) {
+    return {
+      notFound: true
+    };
+  }
+
+
+  const articles = await getArticlesForListing(!!context.preview, pageNumber, context.params?.category ?? 'all');
   const siteMenu = await getSiteMenu(!!context.preview);
   const page = await getItemByCodename<WSL_Page>(pageCodename, !!context.preview);
-  const itemCount = await getArticlesCountByCategory(false, context.params?.category as string)
+  const itemCount = await getArticlesCountByCategory(!!context.preview, selectedCategory);
 
   if (page === null || articles.items.length === 0) {
     return { notFound: true };
@@ -65,10 +74,8 @@ export const getStaticProps: GetStaticProps<Props> = async context => {
 };
 
 export const getStaticPaths = async () => {
-  const categories: string[] = ['all', 'case_study', 'clinical_trial', 'industry_news', 'research'];
-
-  const getAllPagesForCategory = async (category: string) => {
-    const totalCount = category === 'all' ? await getItemsCount(false, 'article') : await getArticlesCountByCategory(false, category);
+  const getAllPagesForCategory = async (category: ArticleTypeWithAll) => {
+    const totalCount = category === 'all' ? await getItemsTotalCount(false, 'article') : await getArticlesCountByCategory(false, category);
     const pagesNumber = Math.ceil((totalCount ?? 0) / ArticlePageSize);
     const pages = Array.from({ length: pagesNumber }).map((_, index) => index + 1);
     return pages.map(pageNumber => ({
@@ -76,7 +83,7 @@ export const getStaticPaths = async () => {
     }));
   };
 
-  const paths = await Promise.all(categories.map(category => getAllPagesForCategory(category)))
+  const paths = await Promise.all(categoryFilterSource.map(category => getAllPagesForCategory(category)))
     .then(categoryPaths => categoryPaths.flat());
   return {
     paths,
