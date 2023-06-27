0d1df51aee97de959cde5f5af6c83032c9afb37b
Ondřej Chrastina <ondrejch@kontent.ai>
2023-06-27 15:18:53 +0200

    use conext.js config redirect

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/articles',
+        destination: `/articles/category/all`,
+        permanent: true,
+      }
+    ]
+  },
 }
