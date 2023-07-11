979836345cfd3de07ed3dc4b73cf3ffd6b27cef9
Daniel Pokorny <danielp@kontent.ai>
2023-07-12 01:39:31 +0200

    fix menu width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import { perCollectionSEOTitle } from '../lib/constants/labels'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-full h-screen">
       <Component {...pageProps} />
       <Head>
         {/* default title */}
