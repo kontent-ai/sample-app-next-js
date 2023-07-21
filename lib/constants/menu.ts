1fd8721538cac7964e99142006ead2079a9974d6
Ondřej Chrastina <ondrejch@kontent.ai>
2023-07-21 12:29:45 +0200

    rewrite filtering for current domain

--- lib/constants/menu.ts
+++ lib/constants/menu.ts
@@ -7,10 +7,9 @@ export const perCollectionRootItems = {
 } as const satisfies PerCollection<string>;
 
 export const externalUrlsMapping = Object.fromEntries(
-  process.env.NEXT_PUBLIC_COLLECTION_DOMAINS?.split(",")
+  process.env.OTHER_COLLECTIONS_DOMAINS?.split(",")
     .map(collectionPair => collectionPair.split(":"))
-    .map(([collectionCodename, domain]) => [collectionCodename, "https://" + domain])
-    .filter(([collectionCodename]) => collectionCodename !== process.env.NEXT_PUBLIC_KONTENT_COLLECTION_CODENAME) ?? []
+    .map(([collectionCodename, domain]) => [collectionCodename, "https://" + domain]) ?? []
 );
 
 export const getRootCodename = (siteCodename: ValidCollectionCodename) => perCollectionRootItems[siteCodename];
--- lib/utils/env.ts
+++ lib/utils/env.ts
@@ -1,10 +1,10 @@
 import { isValidCollectionCodename } from "../types/perCollection";
 
-const { NEXT_PUBLIC_KONTENT_COLLECTION_CODENAME } = process.env;
+const { KONTENT_COLLECTION_CODENAME } = process.env;
 
-if (!isValidCollectionCodename(NEXT_PUBLIC_KONTENT_COLLECTION_CODENAME)) {
-  throw new Error(`Invalid collection codename "${NEXT_PUBLIC_KONTENT_COLLECTION_CODENAME}".`);
+if (!isValidCollectionCodename(KONTENT_COLLECTION_CODENAME)) {
+  throw new Error(`Invalid collection codename "${KONTENT_COLLECTION_CODENAME}".`);
 }
 
 /** Use only on server - for client use `useSiteCodename` hook */
-export const siteCodename = NEXT_PUBLIC_KONTENT_COLLECTION_CODENAME;
+export const siteCodename = KONTENT_COLLECTION_CODENAME;
