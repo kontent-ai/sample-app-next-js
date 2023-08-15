93157c1a4f1d010653ffa77010366c11c2de1c8a
Ivan Kiral <kiralivan@gmail.com>
2023-08-15 09:46:40 +0200

    Add comments explaining middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,17 @@
 import { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
 export const middleware = (request: NextRequest) => {
+  // /articles will be shown in as URL but request will be against /articles/category/all
   if(request.nextUrl.pathname === '/articles'){
     return NextResponse.rewrite(new URL('/articles/category/all', request.url));
   }
-  if(request.nextUrl.pathname.endsWith('category/all')){
+
+  // Redirect to the /articles when manually type the /articles/category/all URL
+  if(request.nextUrl.pathname === '/articles/category/all'){
     return NextResponse.redirect(new URL('/articles', request.url));
   }
+  
   return NextResponse.next();
 }
 
