f24ce6276e0f77aebac19a730c52cf24c3ee3a09
Ivan Kiral <kiralivan@gmail.com>
2023-08-15 14:57:41 +0200

    refactor 404 when articles page 1 or 0 to redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/articles/category/:category/page/([0-1])',
+        destination: '/articles/category/:category',
+        permanent: true,
+      }
+    ]
+  },
   i18n: {
     locales: ['en-US'],
     defaultLocale: 'en-US',
