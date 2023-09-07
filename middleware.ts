b963b1b66dda0d0205b782fb774a6ef32001c6c8
Ondřej Chrastina <ondrejch@kontent.ai>
2023-09-07 15:29:50 +0200

    simplify middleware logic

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,12 @@ import { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 export const middleware = (request: NextRequest) => {
-  // /articles will be shown in as URL but request will be against /articles/category/all
+  // /articles will be shown in as URL but request will be against fully defined URL path
   if (request.nextUrl.pathname === '/articles') {
     return NextResponse.rewrite(new URL('/articles/category/all/page/1', request.url));
   }
 
-  // /articles will be shown in as URL but request will be against /articles/category/all
-  if (request.nextUrl.pathname === '/articles') {
-    return NextResponse.rewrite(new URL('/articles/category/all/page/1', request.url));
-  }
-
-  // Redirect to the /articles when manually type the /articles/category/all URL
-  if (request.nextUrl.pathname === '/articles/category/all') {
-    return NextResponse.redirect(new URL('/articles', request.url));
-  }
-
-  // /articles will be shown in as URL but request will be against /articles/category/all
+  // If there is no pagination, but category provided - add the first page ti URL path
   if (/^\/articles\/category\/[^\/]+$/.test(request.nextUrl.pathname)) {
     return NextResponse.rewrite(new URL(request.url + "/page/1", request.url));
   }
