da237791cb21c2c68ea5f0df0e00fc54af74fbfd
Ivan Kiral <kiralivan@gmail.com>
2023-09-13 09:34:59 +0200

    update middleware to handle articles

--- middleware.ts
+++ middleware.ts
@@ -6,27 +6,18 @@ export const middleware = (request: NextRequest) => {
   if (!currentEnvId) {
     throw new Error("Missing 'NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID' environment variable.");
   }
-
+  
   if(request.nextUrl.pathname === '/envid' ||  request.nextUrl.pathname === '/callback'){
     return NextResponse.next();
   }
 
-  if(request.nextUrl.pathname.startsWith('/_next/static')){
-    return NextResponse.next();
-  }
-
   if(request.nextUrl.pathname === '/articles'){
-    return NextResponse.rewrite(new URL(`/${currentEnvId}/articles/category/all`, request.url));
+    return NextResponse.rewrite(new URL(`/${currentEnvId}/articles/category/all/page/1`, request.url));
   }
 
   // Redirect to the /articles when manually type the /articles/category/all URL
   if(request.nextUrl.pathname === '/articles/category/all'){
     return NextResponse.redirect(new URL('/articles', request.url));
-  }
-
-  // /articles will be shown in as URL but request will be against fully defined URL path
-  if (request.nextUrl.pathname === '/articles') {
-    return NextResponse.rewrite(new URL('/articles/category/all/page/1', request.url));
   }
 
   // If there is no pagination, but category provided - add the first page ti URL path
