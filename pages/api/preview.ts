674005398e3ff8d87beedbdfe5b93fa81f22c26f
Ivan Kiral <kiralivan@gmail.com>
2023-09-14 11:07:31 +0200

    change callback url

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -8,10 +8,12 @@ const handler: NextApiHandler = async (req, res) => {
     return res.status(401).json({ message: 'Invalid preview token, or no slug and type provided.' })
   }
 
-  const {currentPreviewApiKey} = req.cookies
+  const { currentPreviewApiKey } = req.cookies
 
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({currentPreviewApiKey})
+  res.setPreviewData({ currentPreviewApiKey });
+
+
 
   const path = await resolveUrlPath({
     type: req.query.type.toString(),
