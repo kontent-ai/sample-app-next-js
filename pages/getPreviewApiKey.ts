6dab3ffb5412991cf80692571466901fc594d848
Jiri Lojda <lojdaj@seznam.cz>
2023-10-09 16:55:24 +0200

    Use prompt=none for silent login and fallback on auth error

--- pages/getPreviewApiKey.ts
+++ pages/getPreviewApiKey.ts
@@ -1,12 +1,14 @@
+import { setCookie } from "cookies-next";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { FC, useEffect } from "react";
 
 import { webAuth } from "../lib/constants/auth";
+import { urlAfterAuthCookieName } from "../lib/constants/cookies";
 
 const GetPreviewApiKey: FC = () => {
   const router = useRouter();
-  const { path } = router.query;
+  const { path, promptLogin } = router.query;
 
   useEffect(() => {
     if (!router.isReady) { // We need to wait until the component is hydrated, because otherwise router.query is empty (https://nextjs.org/docs/pages/building-your-application/rendering/automatic-static-optimization)
@@ -15,9 +17,10 @@ const GetPreviewApiKey: FC = () => {
     if (!path) {
       console.warn("Missing query parameter 'path' in /getPreviewApiKey. Will redirect to / after auth.");
     }
-    webAuth.authorize({ redirectUri: `${window.origin}/callback`, appState: path });
+    setCookie(urlAfterAuthCookieName, path);
+    webAuth.authorize({ redirectUri: `${window.origin}/callback`, prompt: typeof promptLogin === "string" ? undefined : "none" });
 
-  }, [path, router.isReady])
+  }, [path, router.isReady, promptLogin])
 
   return null;
 }
