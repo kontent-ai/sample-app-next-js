31b66893c940d7b8bef69910a026715d936d6bf2
Ivan Kiral <kiralivan@gmail.com>
2025-01-27 09:42:06 +0100

    remove i18n from config

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ module.exports = {
       }
     ]
   },
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
